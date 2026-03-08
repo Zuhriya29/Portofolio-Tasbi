fa35325de9ae38d1ee7251f55aed03fd599822be
Zuhriya29 <113580042+Zuhriya29@users.noreply.github.com>
2026-03-08 21:40:07 +0700

    edit

--- src/assets/elemen/data/project2data.js
+++ src/assets/elemen/data/project2data.js
@@ -6,10 +6,22 @@ const project2data = [
     design: "https://www.figma.com/design/75ZZ8hjY6lVWn4qkX831Y4/Snapbox?node-id=0-1&t=POjdryG6vkcAa0w6-1"
   },
   {
+    img: "/design-upt.png",
+    title: "UPT Publikasi Ilmiah UM - Website Profile",
+    prototype: "https://www.figma.com/proto/lJvrzNswDu5en4qt1SyRtq/UPT-Publika-UM?node-id=0-1&t=eGBRy73BhhCeZHAF-1",
+    design: "https://www.figma.com/design/lJvrzNswDu5en4qt1SyRtq/UPT-Publika-UM?node-id=0-1&t=eGBRy73BhhCeZHAF-1"
+  },
+  {
     img: "/design-portal-berita.png",
     title: "TrendToday –    News Portal Platform",
     prototype: "https://www.figma.com/proto/KQ0iAcT4vZnKpVEefHKOf7/Portal-Berita?node-id=92-7&t=HfVfXZ7Bnz2U9wry-1",
     design: "https://www.figma.com/design/KQ0iAcT4vZnKpVEefHKOf7/Portal-Berita?node-id=92-7&t=HfVfXZ7Bnz2U9wry-1"
+  },
+  {
+    img: "/design-toko-kue.png",
+    title: "Brownie Cake – Website Ecomerce",
+    prototype: "https://www.figma.com/proto/Vl2asPee6Dg4PEW9ZM6k7o/Web-Toko-Kue?node-id=56-977&t=xuPAe5cw6Vj6T9g8-1 ",
+    design: "https://www.figma.com/design/Vl2asPee6Dg4PEW9ZM6k7o/Web-Toko-Kue?node-id=56-977&t=xuPAe5cw6Vj6T9g8-1"
   },
 ];
 
